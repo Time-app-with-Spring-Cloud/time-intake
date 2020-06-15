0cc2d7f5a2a571b966537e13ee06c5eb23a10b27
Pedro Galan <pedro.galan@piksel.com>
2020-06-15 11:42:53 +0100

    Spring boot app skeleton

---
+++ src/main/java/es/pedrogalan/time/intake/TimeIntakeApplication.java
@@ -0,0 +1,13 @@
+package es.pedrogalan.time.intake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeIntakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeIntakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/pedrogalan/time/intake/TimeIntakeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.pedrogalan.time.intake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeIntakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
